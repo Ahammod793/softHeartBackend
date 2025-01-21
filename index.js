ae7994431df4a455d22c5b5fb80833671da75df3
Ahammod793 <mdahammodali793@gmail.com>
2025-01-21 18:48:28 +0600

    delete operation code supplied

--- index.js
+++ index.js
@@ -87,6 +87,13 @@ async function run() {
 
    
 
+    app.delete('/campaign/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+     const DeleteRes = await campaigns.deleteOne(query)
+      res.send(DeleteRes)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. success MongoDB!");
   } finally {
