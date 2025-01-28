a3b8c57acedcac3a233e02d95ea33c5d1dba353c
Ahammod793 <mdahammodali793@gmail.com>
2025-01-28 19:54:38 +0600

    get the users info so visitors can see homepage graph

--- index.js
+++ index.js
@@ -1,150 +1,154 @@
-require( 'dotenv' ).config()
-const express = require( 'express' )
-const cors = require( 'cors' );
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
-app.use( cors() );
-app.use( express.json() );
 const port = process.env.PORT || 5000;
 
-// Linked to ahammods-projects/soft-heart-backend (created .vercel and added it to .gitignore)
-// Inspect: https://vercel.com/ahammods-projects/soft-heart-backend/7A84zGyvyrK3QFvYTiXNK2Bg4kfx [3s]
-// Production: https://soft-heart-backend-eiw7wl9s5-ahammods-projects.vercel.app [3s]
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-// 
-// console.log(process.env.admin, process.env.pass)
-
-const { MongoClient, ServerApiVersion, ObjectId } = require( 'mongodb' );
-const uri = `mongodb+srv://${ process.env.admin }:${ process.env.pass }@cluster0.j5xue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient( uri, {
+// MongoDB connection
+const uri = `mongodb+srv://${process.env.admin}:${process.env.pass}@cluster0.j5xue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-} );
+  },
+});
 
-async function run ()
-{
-  try
-  {
-    // Connect the client to the server	(optional starting in v4.7)
+// Main function to handle database operations
+async function run() {
+  try {
     await client.connect();
-    // Send a ping to confirm a successful connection
-    // my database 
-    const softHeart = client.db( "softHeart" );
-    const campaigns = softHeart.collection( "Campaign" );
-    app.get( '/', async ( req, res ) =>
-    {
-      const data = campaigns.find()
-      const result = await data.toArray()
-      res.send( result )
-    } )
 
-    app.get( '/:mail', async ( req, res ) =>
-    {
-      const mail = req.params.mail
-      const query = { email: mail }
-      const data = campaigns.find( query )
-      const result = await data.toArray()
-      res.send( result )
-    } )
+    const softHeart = client.db("softHeart");
 
-    app.get( '/updateCamp/:id', async ( req, res ) =>
-    {
+    const campaigns = softHeart.collection("Campaign");
+    const donarsCollection = softHeart.collection("donarsCollection");
+    const usersCollection = softHeart.collection("users");
+
+    // Endpoints
+    app.get('/', async (req, res) => {
+      const result = await campaigns.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/:mail', async (req, res) => {
+      const mail = req.params.mail;
+      const query = { email: mail };
+      const result = await campaigns.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/updateCamp/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId( id ) }
-      const oneData = await campaigns.findOne( query )
-      res.send( oneData )
-    } )
+      const query = { _id: new ObjectId(id) };
+      const result = await campaigns.findOne(query);
+      res.send(result);
+    });
 
-    app.get( '/camp/:id', async ( req, res ) =>
-    {
+    app.patch('/updateCamp/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId( id ) }
-      const result = await campaigns.findOne( query )
-      res.send( result )
-    } )
-
-    app.patch( '/updateCamp/:id', async ( req, res ) =>
-    {
-      const id = req.params.id;
-      const data = req.body
-      const filter = { _id: new ObjectId( id ) }
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          title: data.campaignTitle, campType: data.campaignType, campaignStart: data.campStart, campaignEnd: data.campEnd, campDiscription: data.description,
-          donation: data.donationAmount_, file: data.file, name: data.name, email: data.email,
-        }
-      }
-      const result = await campaigns.updateMany( filter, updateDoc )
-      res.send( result )
-    } )
+          title: data.campaignTitle,
+          campType: data.campaignType,
+          campaignStart: data.campStart,
+          campaignEnd: data.campEnd,
+          campDiscription: data.description,
+          donation: data.donationAmount_,
+          file: data.file,
+          name: data.name,
+          email: data.email,
+        },
+      };
+      const result = await campaigns.updateMany(filter, updateDoc);
+      res.send(result);
+    });
 
-    app.post( '/newcampaign', async ( req, res ) =>
-    {
+    app.post('/newcampaign', async (req, res) => {
       const camp = req.body;
       const doc = {
-        title: camp.campaignTitle, campType: camp.campaignType, campaignStart: camp.campStart, campaignEnd: camp.campEnd, campDiscription: camp.description,
-        donation: camp.donationAmount_, file: camp.file, name: camp.name, email: camp.email,
-      }
-      const result = await campaigns.insertOne( doc );
-      res.send( result )
+        title: camp.campaignTitle,
+        campType: camp.campaignType,
+        campaignStart: camp.campStart,
+        campaignEnd: camp.campEnd,
+        campDiscription: camp.description,
+        donation: camp.donationAmount_,
+        file: camp.file,
+        name: camp.name,
+        email: camp.email,
+      };
+      const result = await campaigns.insertOne(doc);
+      res.send(result);
+    });
 
-    } )
-
-
-
-    app.delete( '/campaign/:id', async ( req, res ) =>
-    {
+    app.delete('/campaign/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId( id ) };
-      const DeleteRes = await campaigns.deleteOne( query )
-      res.send( DeleteRes )
-    } )
+      const query = { _id: new ObjectId(id) };
+      const result = await campaigns.deleteOne(query);
+      res.send(result);
+    });
 
-
-    const donarsCollection = softHeart.collection('donarsCollection')
-
-    app.post( '/donarDetails', async ( req, res ) => {
+    app.post('/donarDetails', async (req, res) => {
       const donationDetails = req.body;
       const doc = {
+        campID: donationDetails.campID,
         donarName: donationDetails.donarName,
         donarMail: donationDetails.donarMail,
         thumb: donationDetails.thumb,
         amount: donationDetails.donatedAmount,
         title: donationDetails.headline,
-        situation: donationDetails.donationSituation
-      }
+        situation: donationDetails.donationSituation,
+        donationDate: donationDetails.donationDate,
+      };
       const result = await donarsCollection.insertOne(doc);
-      res.send(result)
-    } )
-   
+      res.send(result);
+    });
 
-    app.get('/donarDetails/:mail',async(req, res) => {
+    app.get('/donatDetails/getInfo', async (req, res) => {
+      const result = await donarsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/donarDetails/:mail', async (req, res) => {
       const mail = req.params.mail;
-      // console.log(mail)
-      const query = {donarMail : mail}
-      const collection =  donarsCollection.find(query)
-      const result = await collection.toArray(collection)
-      res.send(result)
-    })
+      const query = { donarMail: mail };
+      const result = await donarsCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    app.post('/users', async (req, res) => {
+      const data = req.body;
+      const doc = {
+        Email: data.Email,
+        name: data.name,
+        date : data.date
+      };
+      const result = await usersCollection.insertOne(doc);
+      res.send(result);
+    });
 
+    app.get('/users/info', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-    await client.db( "admin" ).command( { ping: 1 } );
-    console.log( "Pinged your deployment. success MongoDB!" );
-  } finally
-  {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    // Confirm database connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. Connected successfully to MongoDB!");
+  } finally {
+    // Leave client open for persistent connections
   }
 }
-run().catch( console.dir );
+run().catch(console.dir);
 
-
-app.listen( port, () =>
-{
-  console.log( 'your port is ', port )
-} )
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
